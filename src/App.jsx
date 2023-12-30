19264422839bd6c34c063de9d8eceeed0b4bb233
laiba-shakeel <aqsa.shaqil@gmail.com>
2023-12-30 17:37:18 +0500

    simple make this react + vite based assignment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="container-fluid mainDiv">
+        <div className="headerContainer container-fluid">
+          <Header />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
